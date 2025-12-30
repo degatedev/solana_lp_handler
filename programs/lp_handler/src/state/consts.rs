b8354190f0eaf3c01fe06b04d880833273cddd61
yukai <yukai@andui.com>
2025-12-30 20:07:48 +0800

    update fee_address

--- programs/lp_handler/src/state/consts.rs
+++ programs/lp_handler/src/state/consts.rs
@@ -8,6 +8,9 @@ pub const FEE_OWNERS: &[Pubkey] = &[
     pubkey!("J3t7ucPyFEh4QH7ut1nfLe1JKC18NU476tyhxNsQvnqg"), // prod
     pubkey!("E32ykUTbi4Ag8t4Hic41HtDVwAZca1oGorqvkt3YS7Dy"), // dev
     pubkey!("5sMFtms83riv1vq7e2nGtDBN9w14Mvj2uojPNe32fvwE"), // teste
+    pubkey!("ECcfQBco4MLM8Dkztn7HSVyZze2MY3qkv621PYXbkvon"), // testd
+    pubkey!("AyVjdDnjmkcsfLKZxwHVVafFDJ8BFhk5oLy3w9PLtfbY"), // testd
+    pubkey!("5supVqBoki4jARg3EFjFgpP84mXs6PgC2nUwae3iTDcE"), // stg
 ];
 
 pub fn is_fee_owner(owner: &Pubkey) -> bool {

3c980e10a75485efa9c04f6271bb497c10d15cdc
yukai <yukai@andui.com>
2026-01-29 02:21:36 +0800

    cli

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,8 +12,10 @@ export const deploy = async () => {
     ['new', '--no-bip39-passphrase', '--silent', '-o', 'target/deploy/lp_handler-keypair.json', '--force'],
     spawnSyncOptions
   );
-  console.log('合约ID生成成功');
-
+  const secret = Uint8Array.from(JSON.parse(fs.readFileSync('target/deploy/lp_handler-keypair.json', 'utf8')));
+  const programKeypair = Keypair.fromSecretKey(secret);
+  const programId = programKeypair.publicKey;
+  console.log('合约ID生成成功:', programId.toBase58());
   console.log('同步合约ID...');
   execSync('anchor', ['keys', 'sync'], spawnSyncOptions);
   console.log('合约ID同步成功');
@@ -26,9 +28,7 @@ export const deploy = async () => {
   console.log('开始更新安全配置...');
   // IMPORTANT: init using the freshly deployed programId (don't rely on a cached anchor.workspace program)
   const { initSecurityConfig } = await import('./security_config');
-  const secret = Uint8Array.from(JSON.parse(fs.readFileSync('target/deploy/lp_handler-keypair.json', 'utf8')));
-  const programKeypair = Keypair.fromSecretKey(secret);
-  const programId = programKeypair.publicKey;
+
   await initSecurityConfig(programId.toBase58());
   console.log('安全配置更新成功');
 };

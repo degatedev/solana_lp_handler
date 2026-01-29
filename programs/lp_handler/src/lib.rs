7da80dd4f32b7d6c3395004c288132ddbe2fcca4
yukai <yukai@andui.com>
2026-01-29 12:56:59 +0800

    add log detail

--- programs/lp_handler/src/lib.rs
+++ programs/lp_handler/src/lib.rs
@@ -12,7 +12,7 @@ use state::*;
 
 // ProgramId 需要与部署的 program keypair 对应的地址一致。
 // 本分支统一使用生产环境（mainnet）的 ProgramId。
-declare_id!("DNPzTxHx6ortBYLx83nnsG1TeUmkbtqRL41DRAxfZJUS");
+declare_id!("2DVSwnMktqb7hk6iXtrNcirX9XgiSgspHF1LHp7hmmNS");
 
 #[program]
 #[allow(deprecated)]
